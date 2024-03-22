498a61dbc7b5a7a191d77943087ff89bf73a027e
HiYi <hieu243268@gmail.com>
2024-03-22 13:20:55 -0700

    init project

---
+++ bookcar/src/pages/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className=" text-center text-white">
+      <h1>Xe lên tỉnh</h1>
+      <button>Danh sách chuyến xe</button>
+      <br></br>
+      <button>Lọc chuyến đi</button>
+    </div>
+  );
+}
