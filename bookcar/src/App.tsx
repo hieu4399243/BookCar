bd7d4708a4052cdf6deefeadaba3f08d35f70834
HiYi <hieu243268@gmail.com>
2024-03-22 15:02:23 -0700

    main screen

--- bookcar/src/App.tsx
+++ bookcar/src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import Main from './pages/main';
+import React from "react";
+import "./App.css";
+import Main from "./pages/main";
 
 function App() {
   return (
-    <div className=' h-screen'>
-      <Main/>
+    <div className="h-screen bg-[#012f48] text-center">
+      <Main />
     </div>
   );
 }
--- bookcar/src/pages/main.tsx
+++ bookcar/src/pages/main.tsx
@@ -2,11 +2,24 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className=" text-center text-white">
-      <h1>Xe lên tỉnh</h1>
-      <button>Danh sách chuyến xe</button>
-      <br></br>
-      <button>Lọc chuyến đi</button>
+    <div className=" text-center text-white relative">
+      <div className="relative flex justify-center items-center">
+        <div className="opacity-80 h-24 absolute bg-[#224963] w-[400px] rounded-md "></div>
+        <div className="opacity-50 h-40 absolute bg-[#224963] w-[350px] rounded-md"></div>
+        <h1 className="absolute top-16 text-[20px] font-bold uppercase">
+          Xe liên tỉnh
+        </h1>
+        <div className="absolute top-96">
+          <div>
+            <button className="text-[25px] mb-3">
+              Danh sách chuyến đi
+            </button>
+          </div>
+          <div>
+            <button className="text-[25px]">Lọc chuyến xe</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
