f941bd8e2b2119b39e8d78d1ba7e0df1808ef5ed
hieu4399243 <hieu243268@gmail.com>
2024-03-26 22:02:21 +0700

    add border check time , filter name

--- bookcar/src/pages/listCar/time.tsx
+++ bookcar/src/pages/listCar/time.tsx
@@ -18,15 +18,17 @@ export default function Time() {
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
   const [filteredTrips, setFilteredTrips] = useState<Trip[]>([]);
   const [priceRange, setPriceRange] = useState([0, 3000000]);
-  const [selectedTrips, setSelectedTrips] = useState<string[]>([]);
+  const [selectedGarage, setSelectedGarage] = useState<string[]>([]);
   const [selectedVehicleNames, setSelectedVehicleNames] = useState<string[]>(
     []
   );
   const [showAllData, setShowAllData] = useState<boolean>(true);
+  const [clickedOption, setClickedOption] = useState<string | null>(null);
 
   useEffect(() => {
     if (selectedTime && !showAllData) {
       const tripsInSelectedTime = (groupedTrips[selectedTime] as Trip[]).flat();
+      console.log(tripsInSelectedTime);
       const filteredTripsByPrice = tripsInSelectedTime.filter((trip: Trip) => {
         return (
           trip.discount_amount >= priceRange[0] &&
@@ -34,6 +36,7 @@ export default function Time() {
         );
       });
       setFilteredTrips(filteredTripsByPrice);
+      console.log(setFilteredTrips(filteredTripsByPrice));
     }
   }, [selectedTime, priceRange, showAllData]);
 
@@ -42,6 +45,7 @@ export default function Time() {
     if (time) {
       setSelectedTime(time);
       setShowAllData(false);
+      setClickedOption(time === clickedOption ? null : time);
       const tripsInSelectedTime = (groupedTrips[time] as Trip[]).flat();
       setFilteredTrips(tripsInSelectedTime);
       let minPrice = Number.POSITIVE_INFINITY;
@@ -54,43 +58,43 @@ export default function Time() {
     } else {
       setSelectedTime(null);
       setShowAllData(true);
+      setClickedOption(null);
     }
   };
 
-  const handleCheckboxChange = (uuid: string) => {
-    const trip = filteredTrips.find((trip) => trip.uuid === uuid);
-    console.log(trip);
-    if (trip) {
-      const index = selectedTrips.indexOf(uuid);
-      if (index !== -1) {
-        setSelectedTrips(selectedTrips.filter((item) => item !== uuid));
-        setSelectedVehicleNames(
-          selectedVehicleNames.filter((name) => name !== trip.vehicle_name)
-        );
-      } else {
-        setSelectedTrips([...selectedTrips, uuid]);
-        setSelectedVehicleNames([...selectedVehicleNames, trip.vehicle_name]);
-      }
-    }
-  };
+  // const handleCheckboxChange = (uuid: string) => {
+  //   const trip = filteredTrips.find((trip) => trip.uuid === uuid);
+  //   console.log(trip);
+  //   if (trip) {
+  //     const index = selectedTrips.indexOf(uuid);
+  //     if (index !== -1) {
+  //       setSelectedTrips(selectedTrips.filter((item) => item !== uuid));
+  //       setSelectedVehicleNames(
+  //         selectedVehicleNames.filter((name) => name !== trip.vehicle_name)
+  //       );
+  //     } else {
+  //       setSelectedTrips([...selectedTrips, uuid]);
+  //       setSelectedVehicleNames([...selectedVehicleNames, trip.vehicle_name]);
+  //     }
+  //   }
+  // };
 
-  const uniqueVehicleNames =
-    filteredTrips.length > 0
-      ? Array.from(new Set(filteredTrips.map((trip) => trip.vehicle_name)))
-      : [];
+  // const uniqueVehicleNames =
+  //   filteredTrips.length > 0
+  //     ? Array.from(new Set(filteredTrips.map((trip) => trip.vehicle_name)))
+  //     : [];
   const uniqueTransportName =
     filteredTrips.length > 0
       ? Array.from(
           new Set(filteredTrips.map((trip) => trip.transport_information.name))
         )
       : [];
-
+     console.log(uniqueTransportName);
   const handleCancel = () => {
     setSelectedTime(null);
     setShowAllData(true);
     setFilteredTrips([]);
     setPriceRange([0, 3000000]);
-    setSelectedTrips([]);
     setSelectedVehicleNames([]);
   };
 
@@ -101,7 +105,7 @@ export default function Time() {
       <div className="grid grid-cols-2">
         <div
           className={`time-option ${
-            selectedTime === "morning" ? "selected" : ""
+            clickedOption === "morning" ? "selected" : ""
           }`}
           onClick={handleFilter}
           data-time="morning"
@@ -110,7 +114,9 @@ export default function Time() {
           <p>0:00 - 6:00</p>
         </div>
         <div
-          className="bg-white p-3 m-3 rounded-md"
+          className={`time-option ${
+            clickedOption === "noon" ? "selected" : ""
+          }`}
           onClick={handleFilter}
           data-time="noon"
         >
@@ -118,7 +124,9 @@ export default function Time() {
           <p>6:01 - 12:00</p>
         </div>
         <div
-          className="bg-white p-3 m-3 rounded-md"
+          className={`time-option ${
+            clickedOption === "afternoon" ? "selected" : ""
+          }`}
           onClick={handleFilter}
           data-time="afternoon"
         >
@@ -126,7 +134,9 @@ export default function Time() {
           <p>12:01 - 18:00</p>
         </div>
         <div
-          className="bg-white p-3 m-3 rounded-md"
+          className={`time-option ${
+            clickedOption === "evening" ? "selected" : ""
+          }`}
           onClick={handleFilter}
           data-time="evening"
         >
@@ -146,19 +156,18 @@ export default function Time() {
             filteredTrips.length > 0 ? (
               <div className="garage-list">
                 <ul>
-                  {filteredTrips.map((trip) => (
-                    <li key={trip.uuid} className="garage-list-item">
+                  {uniqueTransportName.map((name) => (
+                    <li key={name} className="garage-list-item">
                       <div>
-                        <p>{trip.transport_information.name}</p>
+                        <p>{name}</p>
                       </div>
                       <div className="round">
                         <input
-                          id={`checkbox-${trip.uuid}`}
                           type="checkbox"
-                          checked={selectedTrips.includes(trip.uuid)}
-                          onChange={() => handleCheckboxChange(trip.uuid)}
+                          checked={true}
+                          readOnly
                         />
-                        <label htmlFor={`checkbox-${trip.uuid}`}></label>
+                        <label></label>
                       </div>
                     </li>
                   ))}
@@ -180,8 +189,7 @@ export default function Time() {
                         <input
                           id={`checkbox-${trip.uuid}`}
                           type="checkbox"
-                          checked={selectedTrips.includes(trip.uuid)}
-                          onChange={() => handleCheckboxChange(trip.uuid)}
+                          checked={true}
                         />
                         <label htmlFor={`checkbox-${trip.uuid}`}></label>
                       </div>
@@ -204,7 +212,7 @@ export default function Time() {
                 {filteredTrips.map((trip) => (
                   <li key={trip.uuid} className="category-list-item">
                     <div>
-                      {selectedTrips.includes(trip.uuid) && (
+                      {selectedGarage.includes(trip.uuid) && (
                         <p>Phương tiện: {trip.vehicle_name}</p>
                       )}
                     </div>
@@ -222,7 +230,7 @@ export default function Time() {
           Xoá lọc
         </button>
         <button className="button-right">
-          Áp dụng({selectedTrips.length})
+          {/* Áp dụng({selectedTrips.length}) */}
         </button>
       </div>
     </div>
